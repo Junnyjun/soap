440ba503b4ff2d5377f1a9411cf67a814c78c9dc
junny <chbe5082@naver.com>
2024-01-06 14:43:54 +0900

    init

---
+++ src/test/kotlin/git/io/soap/SoapApplicationTests.kt
@@ -0,0 +1,13 @@
+package git.io.soap
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoapApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
